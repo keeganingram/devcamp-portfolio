fa2e6ca9f396d639c048137651ddccc8ac0a968e
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-20 15:37:24 +0000

    Implemented custom action for updating statuses for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   
   get 'contact-me', to: 'pages#contact'
 
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs do
+    member do
+      get :toggle_status
+  end
+end
+ 
   root to: 'pages#home'
 end
